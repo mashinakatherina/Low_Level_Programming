75883588aaac2d2a87468316f5c5b69d6cc7ccf1
Katerina Mashina <mashina.katherina@gmail.com>
2020-10-22 12:37:26 +0300

    mb done

---
+++ Lab4/utilites.h
@@ -0,0 +1,19 @@
+//
+// Created by Katherina on 18.10.2020.
+//
+
+#include "./linked_list.h"
+#include <stdbool.h>
+
+#ifndef H_UTIL
+#define H_UTIL
+
+bool save(struct LinkedList *list, const char *filename);
+
+bool load(struct LinkedList **list, const char *filename);
+
+bool serialize(struct LinkedList *list, const char *filename);
+
+bool deserialize(struct LinkedList **list, const char *filename);
+
+#endif
