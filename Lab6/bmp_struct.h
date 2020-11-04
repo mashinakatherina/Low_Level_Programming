f42a7345d9955b0a5b0e47b631f2a12a616dc551
Katerina Mashina <mashina.katherina@gmail.com>
2020-11-04 16:47:48 +0300

    lab done

---
+++ Lab6/bmp_struct.h
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#ifndef AAA_BMP_STRUCT_H
+#define AAA_BMP_STRUCT_H
+
+struct __attribute__((packed)) bmp_header {
+    uint16_t bfType;
+    uint32_t bfileSize;
+    uint32_t bfReserved;
+    uint32_t bOffBits;
+
+    uint32_t biSize;
+    uint32_t biWidth;
+    uint32_t biHeight;
+    uint16_t biPlanes;
+    uint16_t biBitCount;
+    uint32_t biCompression;
+    uint32_t biSizeImage;
+    uint32_t biXPelsPerMeter;
+    uint32_t biYPelsPerMeter;
+    uint32_t biClrUsed;
+    uint32_t biClrImportant;
+};
+
+struct pixel {
+    unsigned char r, g, b;
+};
+
+struct image {
+    uint32_t width, height;
+    struct pixel *data;
+};
+
+#endif
